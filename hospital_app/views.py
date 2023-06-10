1b7f0291e543aebb514f927cce9adaa4162b7a9f
keerthu000 <keerthanavadath@gmail.com>
2023-06-10 18:03:26 +0530

    first commit

--- hospital_app/views.py
+++ hospital_app/views.py
@@ -7,8 +7,8 @@ from django.contrib.auth import authenticate,login,logout
 # Create your views here.
 def index(request):
     return render(request,'index.html')
-def signup(request):
-    return render(request,'signup.html')
+def sign(request):
+    return render(request,'sign.html')
 @login_required(login_url='index')
 def loginpage(request):
     return render(request,'loginpage.html')
@@ -18,7 +18,7 @@ def register(request):
         print(firstname)
         lastname=request.POST['lname']
         dob=request.POST['date']
-        gender=request.POST['inlineRadioOptions']
+        gender=request.POST['sel']
         email=request.POST['email']
         contact_num=request.POST['number']
         user=User.objects.create_user(username=firstname,first_name=firstname,last_name=lastname,email=email,password=email)  
@@ -39,7 +39,7 @@ def appoinment(request):
             return redirect('loginpage')
         else:
             messages.info(request,'Please Register to make an appointment.')
-            return redirect('signup')
+            return redirect('sign')
                
         
     else:
